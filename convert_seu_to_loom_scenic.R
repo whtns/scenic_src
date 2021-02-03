41f178e8e5735ead3f22078af5d6214d50121158
whtns <kevin.stachelek@gmail.com>
2021-02-02 16:08:24 -0800

    consolidate to run_scenic

--- convert_seu_to_loom_scenic.R
+++ convert_seu_to_loom_scenic.R
@@ -1,37 +1,58 @@
+#!/usr/bin/env Rscript
+args = commandArgs(trailingOnly=TRUE)
+
+if (length(args)==0) {
+    stop("At least one argument must be supplied (input file).n", call.=FALSE)
+} else if (length(args)==1) {
+    # default input file
+    # args[2] = "../output/seurat/unfiltered_seu.rds"
+}
+
 library(seuratTools)
 library(sceasy)
 library(reticulate)
+library(fs)
 
 loompy <- reticulate::import('loompy')
 scanpy <- reticulate::import('scanpy')
 
 # dataset <- "allfeatures_only_pt_seu"
-dataset <- "allfeatures_only_pt_w_ctrl_seu"
+# dataset <- "allfeatures_only_pt_w_ctrl_seu"
+seu_path <- args[1]
+dataset <- fs::path(seu_path)
 
-seu <- readRDS(paste0("output/seurat/", dataset, ".rds"))
+adata_path <- gsub("seurat", "scenic", path_ext_set(seu_path, "h5ad"))
+loom_path <- gsub("seurat", "scenic", path_ext_set(seu_path, "loom"))
+# print(dataset)
+
+seu <- readRDS(seu_path)
 
 # kept_metadata <- c("nCount_RNA", "nFeature_RNA", "sample_id")
-# 
+#
 # kept_metadata <- c("nCount_RNA", "nFeature_RNA", "sample_id", "batch",
 #   "integrated_snn_res.0.2", "integrated_snn_res.0.4", "integrated_snn_res.0.6", "integrated_snn_res.0.8",
 #   "integrated_snn_res.1.2", "integrated_snn_res.1.4", "integrated_snn_res.1.6",
 #   "integrated_snn_res.1.8", "integrated_snn_res.2", "integrated_snn_res.1",
 #   "S.Score", "G2M.Score", "Phase")
-# 
-# seu$gene@meta.data <- seu$gene@meta.data[,kept_metadata]
+#
+# seu@meta.data <- seu@meta.data[,kept_metadata]
 
-for(j in 1:ncol(seu$gene@meta.data)){
-    if(is.factor(seu$gene@meta.data[,j]) == T){
-        seu$gene@meta.data[,j] = as.character(seu$gene@meta.data[,j]) # Force the variable type to be character
-        seu$gene@meta.data[,j][is.na(seu$gene@meta.data[,j])] <- "NA"
+for(j in 1:ncol(seu@meta.data)){
+    if(is.factor(seu@meta.data[,j]) == T){
+        seu@meta.data[,j] = as.character(seu@meta.data[,j]) # Force the variable type to be character
+        seu@meta.data[,j][is.na(seu@meta.data[,j])] <- "NA"
     }
-    if(is.character(seu$gene@meta.data[,j]) == T){
-        seu$gene@meta.data[,j][is.na(seu$gene@meta.data[,j])] <- "NA"
+    if(is.character(seu@meta.data[,j]) == T){
+        seu@meta.data[,j][is.na(seu@meta.data[,j])] <- "NA"
     }
 }
 
-sceasy::convertFormat(seu$gene, from="seurat", to="anndata",
-                      outFile=paste0("output/scenic/", dataset, ".h5ad"))
+message("converting")
 
-adata <- scanpy$read(paste0("output/scenic/", dataset, ".h5ad"))
-adata$write_loom(paste0("output/scenic/", dataset, ".loom"))
+seu <- Seurat::RenameAssays(seu, gene = "RNA")
+
+sceasy::convertFormat(seu, from="seurat", to="anndata",
+                      outFile=adata_path)
+
+adata <- scanpy$read(adata_path)
+adata$write_loom(loom_path)
--- run_scenic.sh
+++ run_scenic.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
-dataset=$1
+seu_path=$1
+dataset=`basename $seu_path .rds`
+
+proj_dir=`realpath $seu_path | xargs dirname | xargs dirname`
+
+input_loom_path=`echo $proj_dir/scenic/$dataset.loom`
+output_loom_path=`echo $proj_dir/scenic/$dataset-final.loom`
+
+# echo $input_loom_path
+
+script_path=`realpath $0 | xargs dirname`
+# echo $script_path
+
+Rscript "$script_path"/convert_seu_to_loom_scenic.R $seu_path
 
 nextflow run aertslab/SCENICprotocol \
         -profile docker \
-        --loom_input ../../output/scenic/"$dataset".loom \
-        --loom_output ../../output/scenic/"$dataset"-final.loom \
+        --loom_input "$input_loom_path" \
+        --loom_output "$output_loom_path" \
         --TFs ~/Homo_sapiens/scenic/hs_hgnc_curated_tfs.txt \
         --motifs ~/Homo_sapiens/scenic/motifs-v9-nr.hgnc.tbl \
         --db ~/Homo_sapiens/scenic/hg38_refseq-10kb_up_and_down_tss.mc9nr.feather \
