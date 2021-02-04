6e7c7c10d3566c58e5fb0f58aedcb56f2e0c1a46
whtns <kevin.stachelek@gmail.com>
2021-02-04 13:07:52 -0800

    run custom dataset

--- run_scenic.sh
+++ run_scenic.sh
@@ -17,20 +17,20 @@ script_path=`realpath $0 | xargs dirname`
 
 Rscript "$script_path"/convert_seu_to_loom_scenic.R $seu_path
 
-# nextflow run aertslab/SCENICprotocol \
-#         -profile docker \
-#         --loom_input "$input_loom_path" \
-#         --loom_output "$output_loom_path" \
-#         --TFs ~/Homo_sapiens/scenic/hs_hgnc_curated_tfs.txt \
-#         --motifs ~/Homo_sapiens/scenic/motifs-v9-nr.hgnc.tbl \
-#         --db ~/Homo_sapiens/scenic/hg38_refseq-10kb_up_and_down_tss.mc9nr.feather \
-#         --thr_min_genes 1
-
 nextflow run aertslab/SCENICprotocol \
-    -profile docker \
-    --loom_input scenic_src/example/expr_mat_tiny.loom \
-    --loom_output "$output_loom_path" \
-    --TFs scenic_src/example/test_TFs_tiny.txt \
-    --motifs scenic_src/example/motifs.tbl \
-    --db scenic_src/example/*feather \
-    --thr_min_genes 1
+        -profile docker \
+        --loom_input "$input_loom_path" \
+        --loom_output "$output_loom_path" \
+        --TFs ~/Homo_sapiens/scenic/hs_hgnc_curated_tfs.txt \
+        --motifs ~/Homo_sapiens/scenic/motifs-v9-nr.hgnc.tbl \
+        --db ~/Homo_sapiens/scenic/hg38_refseq-10kb_up_and_down_tss.mc9nr.feather \
+        --thr_min_genes 1
+
+# nextflow run aertslab/SCENICprotocol \
+#     -profile docker \
+#     --loom_input scenic_src/example/expr_mat_tiny.loom \
+#     --loom_output "$output_loom_path" \
+#     --TFs scenic_src/example/test_TFs_tiny.txt \
+#     --motifs scenic_src/example/motifs.tbl \
+#     --db scenic_src/example/*feather \
+#     --thr_min_genes 1
