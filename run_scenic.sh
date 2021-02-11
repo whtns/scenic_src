1a24088820af2dce6e323f644759895282180b78
whtns <kevin.stachelek@gmail.com>
2021-02-11 13:44:53 -0800

    add copy to run_scenic

--- run_scenic.sh
+++ run_scenic.sh
@@ -8,14 +8,15 @@ proj_dir=`realpath $seu_path | xargs dirname | xargs dirname`
 
 input_loom_path=`echo $proj_dir/scenic/$dataset.loom`
 
-output_loom_path=`echo "$proj_dir"/scenic/$dataset-final.loom`
+output_loom=`echo $dataset-final.loom`
+output_loom_path=`echo "$proj_dir"/scenic/$output_loom`
 
 script_path=`realpath $0 | xargs dirname`
 # echo $script_path
 
 echo "$output_loom_path"
 
-Rscript "$script_path"/convert_seu_to_loom_scenic.R $seu_path
+# Rscript "$script_path"/convert_seu_to_loom_scenic.R $seu_path
 
 echo "$input_loom_path"
 echo "$output_loom_path"
@@ -23,19 +24,21 @@ echo "$output_loom_path"
 nextflow run aertslab/SCENICprotocol \
         -profile docker \
         --loom_input "$input_loom_path" \
-        --loom_output "$output_loom_path" \
+        --loom_output "$output_loom" \
         --TFs ~/Homo_sapiens/scenic/hs_hgnc_curated_tfs.txt \
         --motifs ~/Homo_sapiens/scenic/motifs-v9-nr.hgnc.tbl \
         --db ~/Homo_sapiens/scenic/hg38_refseq-10kb_up_and_down_tss.mc9nr.feather \
         --thr_min_genes 1
 
 
-# for testing
+# # for testing file paths and script 
 # nextflow run aertslab/SCENICprotocol \
 #     -profile docker \
 #     --loom_input scenic_src/example/expr_mat_tiny.loom \
-#     --loom_output "$output_loom_path" \
+#     --loom_output "$output_loom" \
 #     --TFs scenic_src/example/test_TFs_tiny.txt \
 #     --motifs scenic_src/example/motifs.tbl \
 #     --db scenic_src/example/*feather \
 #     --thr_min_genes 1
+
+cp $output_loom $output_loom_path
